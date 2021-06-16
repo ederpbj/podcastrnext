a83647343d4ac6ebd29088fd1f00e84856607796
ederpbj <ederpbj@gmail.com>
2021-06-15 23:40:57 -0300

    Player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/global.scss';
+
+import {Header} from '../components/Header';
+import {Player} from '../components/'';
+
+import styles from '../styles/app.module.scss';
+
 //__app fica por volta de toda aplicação
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+
+    </div>
+  )
 }
 
 export default MyApp
